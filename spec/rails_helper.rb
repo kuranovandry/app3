71ad5d206af682a78360445deea15b4f145a2220
Andrej Kuranov <a.kuranov@svitla.com>
2015-04-24 17:50:03 +0300

    Test user controller and use pg instead sqlite3

---
+++ spec/rails_helper.rb
@@ -0,0 +1,28 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require 'spec_helper'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+# Add additional requires below this line. Rails is not loaded until this point!
+
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that end
+# in _spec.rb will both be required and run as specs, causing the specs to be
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.use_transactional_fixtures = true
+
+  config.infer_spec_type_from_file_location!
+  config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, type: :controller
+  config.include Warden::Test::Helpers
+  config.render_views
+  config.before(:all) do
+    FactoryGirl.reload
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require_relative 'support/controller_helpers'
+require 'devise'
+
+RSpec.configure do |config|
+  config.include ControllerHelpers, type: :controller
+  Warden.test_mode!
+
+  config.after do
+    Warden.test_reset!
+  end
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
