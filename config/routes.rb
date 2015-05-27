d11ee66c4aa3cdd59e681903aa79fbf424be6b2f
bodikqlar <qlarmore@gmail.com>
2015-05-27 16:37:02 +0300

    add service for moving and suming daily statistic to monthly statistic

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   resources :projects
   resources :movies do
     resources :uploads
+    resources :daily_statistics, only: :index
+    resources :monthly_statistics, only: :index
   end
   resources :categories
   root :to => 'users#index'
